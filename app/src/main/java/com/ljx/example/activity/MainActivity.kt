0368fa3cfbe55e7c826dcecba1fa22da7906fa14
liujingxing <18268800083ljx@gmail.com>
2022-07-06 23:00:24 +0800

    增加测试案例

--- app/src/main/java/com/ljx/example/activity/MainActivity.kt
+++ app/src/main/java/com/ljx/example/activity/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.ljx.example.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.ljx.example.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun test(main: com.ljx.example.activity.MainActivity) {
+        Log.d("LJX", main::class.java.name)
     }
 }
